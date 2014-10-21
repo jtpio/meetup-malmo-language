7d50e5498a16e010bf2ef35271532a6f70ee5954
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2014-10-21 22:43:57 +0200

    Fixed some indentation

--- src/home.js
+++ src/home.js
@@ -1,64 +1,64 @@
 'use strict';
 
 var path = require('path');
-var	url = require('url');
+var url = require('url');
 var request = require('request');
 var analyzer = require(path.resolve(__dirname, './analyzer.js'))();
 
 var malmo = {'question': 2187832, 'url': 'intercambio3'};
 
-var MEETUP_KEY = process.env.MEETUP_KEY,
-	QUESTION_ID = malmo.question,
-	REFRESH_TIME = 1000 * 60 * 30;
+var MEETUP_KEY = process.env.MEETUP_KEY;
+var QUESTION_ID = malmo.question;
+var REFRESH_TIME = 1000 * 60 * 30;
 
 var urlObj = {
-	protocol: 'https',
-	host: 'api.meetup.com',
-	pathname: '/2/profiles',
-	query: {
-		group_urlname: malmo.url,
-		sign: true,
-		key: MEETUP_KEY
-	}
+    protocol: 'https',
+    host: 'api.meetup.com',
+    pathname: '/2/profiles',
+    query: {
+        group_urlname: malmo.url,
+        sign: true,
+        key: MEETUP_KEY
+    }
 };
 
 var dest = url.format(urlObj);
 
 function crawl(destUrl) {
 
-	request.get(destUrl, function (err, response, body) {
-		if (err) {
-			console.log(err);
-			return;
-		}
-		var res = JSON.parse(body);
-		var results = res.results;
+    request.get(destUrl, function (err, response, body) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        var res = JSON.parse(body);
+        var results = res.results;
 
-		for (var i = 0; i < results.length; i++) {
-			var answers = results[i].answers;
-			for (var j = 0; j < answers.length; j++) {
-				if (answers[j].question_id === QUESTION_ID) {
-					analyzer.extractLanguage(answers[j].answer);
-				}
-			}
-		}
+        for (var i = 0; i < results.length; i++) {
+            var answers = results[i].answers;
+            for (var j = 0; j < answers.length; j++) {
+                if (answers[j].question_id === QUESTION_ID) {
+                    analyzer.extractLanguage(answers[j].answer);
+                }
+            }
+        }
 
-		if (res.meta.hasOwnProperty('next') && res.meta.next !== '') {
-			crawl(res.meta.next);
-		} else {
-			analyzer.printResults();
-		}
-	});
+        if (res.meta.hasOwnProperty('next') && res.meta.next !== '') {
+            crawl(res.meta.next);
+        } else {
+            analyzer.printResults();
+        }
+    });
 }
 
 crawl(dest);
 
 setInterval(function () {
-	analyzer.init();
-	crawl(dest);
+    analyzer.init();
+    crawl(dest);
 }, REFRESH_TIME);
 
 
 module.exports.getLanguages = function (req, res) {
-	res.send(analyzer.getResults());
+    res.send(analyzer.getResults());
 };
